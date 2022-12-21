04fafabb84ce8dd9a3db80c3e41981bb7c5c051d
Daniel <62781672+dwernz@users.noreply.github.com>
2022-12-21 10:14:59 -0500

    Add project files.

---
+++ Flights/ClientApp/src/app/search-flights/search-flights.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Time } from "@angular/common";
+import { FlightService } from "./../api/services/flight.service";
+import { FlightRm } from "../api/models";
+
+@Component({
+  selector: 'app-search-flights',
+  templateUrl: './search-flights.component.html',
+  styleUrls: ['./search-flights.component.css']
+})
+export class SearchFlightsComponent implements OnInit {
+
+  searchResult: FlightRm[] = []
+
+  constructor(private flightService: FlightService) { }
+
+  ngOnInit(): void {
+  }
+
+  search() {
+    this.flightService.searchFlight({})
+      .subscribe(response => this.searchResult = response,
+        this.handleError);
+  }
+
+  private handleError(err: any) {
+    console.log(err);
+  }
+}
+
